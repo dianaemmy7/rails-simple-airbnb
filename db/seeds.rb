8ce6dc9e2585d3669cf1ad0ed874d6d5663c4d32
Diana Schoeller <diana.schoeller@student.unisg.ch>
2019-09-03 17:46:44 +0300

    some progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 5 fake flats...'
+
+flats_attributes = [
+  {
+    name: 'Light & Spacious Garden Flat London',
+    address: '10 Clifton Gardens London W9 1DT',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: rand(60..500),
+    number_of_guests: rand(1..5)
+  },
+  {
+    name: 'Miami Mansion',
+    address: Faker::Address.street_address,
+    description: Faker::TvShows::Friends.quote,
+    price_per_night: rand(60..500),
+    number_of_guests: rand(1..5)
+  },
+  {
+    name: 'Light & Spacious Garden Flat London',
+    address: Faker::Address.street_address,
+    description: Faker::TvShows::Friends.quote,
+    price_per_night: rand(60..500),
+    number_of_guests: rand(1..5)
+  },
+  {
+    name: 'Light & Spacious Garden Flat London',
+    address: Faker::Address.street_address,
+    description: Faker::TvShows::Friends.quote,
+    price_per_night: rand(60..500),
+    number_of_guests: rand(1..5)
+  }
+]
+
+Flat.create!(flats_attributes)
+
+puts 'finished!'
